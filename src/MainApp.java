22c3acf4c9190192e277aa168be23a2fd463cd38
Danni4421 <85418881+Danni4421@users.noreply.github.com>
2023-04-28 15:42:59 +0700

    get JSON data

---
+++ src/MainApp.java
@@ -0,0 +1,10 @@
+import library.Library;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
+public class MainApp {
+    public static void main(String[] args) throws IOException, ParseException {
+        Library lib = new Library();
+    }
+}
--- src/library/Library.java
+++ src/library/Library.java
@@ -1,5 +1,41 @@
 package library;
 
-public class Library {
+import accounts.AccountManage;
+import books.BooksManage;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Library {
+    protected boolean libraryStatus;
+    protected AccountManage manageAccounts;
+    protected BooksManage manageBooks;
+
+    public Library () throws IOException, ParseException {
+        libraryStatus = true;
+        parseJsonData();
+    }
+
+    private void parseJsonData () throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        FileReader reader = new FileReader(".\\json-files\\data.json");
+        Object resObj = parser.parse(reader);
+        JSONObject resultParse = (JSONObject) resObj;
+        JSONObject getData = (JSONObject) resultParse.get("data");
+        addJSONAccounts((JSONArray) getData.get("accounts"));
+        addJSONBooks((JSONArray) getData.get("books"));
+    }
+
+    private void addJSONAccounts(JSONArray el) {
+
+    }
+
+    private void addJSONBooks(JSONArray el) {
+
+    }
 }
